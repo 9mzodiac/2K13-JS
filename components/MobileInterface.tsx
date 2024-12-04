37b374f6e194dde54bd1f25a7dc5008898554bd8
ZODIAC <90470284+9mzodiac@users.noreply.github.com>
2024-12-04 12:07:19 -0500

    Update MobileInterface.tsx

--- components/MobileInterface.tsx
+++ components/MobileInterface.tsx
@@ -104,7 +104,7 @@ const MobileInterface: React.FC<any> = ({
                 <Image
                   src={lockscreen}
                   layout="fill"
-                  objectFit="cover"
+                  objectFit="fit"
                   className="index-behind"
                   priority
                   blurDataURL={lockscreen}
